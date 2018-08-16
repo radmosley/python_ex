a42ae6e24d49988d2b70fea31a5ce2c014bde2fa
RMosley <ronnie.j.mosley@gmail.com>
2018-08-15 22:15:22 -0700

    Practice with Classes

---
+++ classes.py
@@ -0,0 +1,43 @@
+import random
+
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def show (self):
+        print('{} of {}'.format(self.suit, self.value))
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.build()
+    
+    def build(self):
+        suit = ['spade ', 'club', 'diamond', 'heart']
+        for s in suit:
+            for v in range(1,14):
+                self.cards.append(Card(s, v))
+
+    def show(self):
+        for c in self.cards:
+            c.show()
+    
+    def shuffle(self):
+        for i in range(len(self.cards)-1, 0, -1):
+            r = random.randint(0, i)
+            self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+
+    def draw(self):
+        return self.cards.pop()
+
+
+deck = Deck()
+# card_1 = Card()
+# card_1.show()
+# deck.show()
+deck.shuffle()
+# deck.show()
+
+card = deck.draw()
+card.show()
---
+++ linkedlist.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, head, length):
+        self.head = None
+        self.length = 0
+
+    def push(value):
+        node_1 = Node(value)
+        return node_1
