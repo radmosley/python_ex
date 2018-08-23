60f88c806507d202fe171d7db4673533741a3f35
RMosley <ronnie.j.mosley@gmail.com>
2018-08-22 20:53:20 -0700

    search algo update
    added Hash tables

--- Data Structures/indexEqualsValue.py
+++ Data Structures/indexEqualsValue.py
@@ -1,16 +1,16 @@
-a = [-8,0,2,5]
+array = [-8,0,2,5]
+array2 = [-8,1,3,5]
+array3 = [-8,0,3,5]
 
-def index_equals_value(a):
+def index_equals_value(array):
     index = 0
-    for i in a:
-        if index != i:
+    for a in array:
+        if a != index:
             index += 1
-            
-        
-        
-        else: 
-            if index == i:
-                print(index)
-    else:
-        print(-1)
-index_equals_value(a)
+        else:
+            return index
+    return -1
+
+# print(index_equals_value(array))
+# print(index_equals_value(array2))
+# print(index_equals_value(array3))
